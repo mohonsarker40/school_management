fa9422a52694939c8b1aa99549cb79f5afc5863d
mohon sarker <mohonsarker40@gmail.com>
2024-12-22 16:13:49 +0600

    classes side Nav

--- resources/views/layouts/sideNav.blade.php
+++ resources/views/layouts/sideNav.blade.php
@@ -47,6 +47,21 @@
             </li>
         @endif
 
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#components-nav2" data-bs-toggle="collapse" href="#">
+                    <i class="bi bi-square-fill"></i><span>Academics</span><i class="bi bi-chevron-down
+                    ms-auto"></i>
+                </a>
+                <ul id="components-nav2" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                    <li>
+                        <a href="{{ url('admin/admin/list') }}">
+                            <i class="bi bi-circle"></i><span>Classes</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </li>
+
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ url('logout') }}">
                 <i class="bi bi-box-arrow-right"></i>
