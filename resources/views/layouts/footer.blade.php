f4deee50fefe16679f30145750779cec92600bfb
mohon sarker <mohonsarker40@gmail.com>
2025-05-15 16:41:18 +0600

    update design

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,8 @@
 <footer id="footer" class="footer">
-    <div class="copyright">
-        © Copyright <strong><span>
+    <div class="copyright">© Copyright <strong>
+            <span>
                 <a href="https://github.com/mohonsarker40" target="_blank">Mohon Sarker</a>
-            </span></strong>. All Rights Reserved 2024
+            </span></strong>
+        .All Rights Reserved 2024
     </div>
 </footer>
